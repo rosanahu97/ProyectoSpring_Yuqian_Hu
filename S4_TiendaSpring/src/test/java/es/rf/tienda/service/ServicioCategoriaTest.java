49bfbdd0222b4172eb8c3d5c3b77aa9931a82e5f
Yuqian <rosanahu97@gmail.com>
2023-02-10 22:36:38 +0100

    Test clase ServicioCategoria

--- S4_TiendaSpring/src/test/java/es/rf/tienda/service/ServicioCategoriaTest.java
+++ S4_TiendaSpring/src/test/java/es/rf/tienda/service/ServicioCategoriaTest.java
@@ -1,12 +1,13 @@
 package es.rf.tienda.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 
 import es.rf.tienda.dominio.Categoria;
 import es.rf.tienda.exception.DomainException;
@@ -14,50 +15,60 @@ import es.rf.tienda.interfacesDaos.ICategoriaRepo;
 
 class ServicioCategoriaTest {
 	
-	@Mock
+	
 	private ICategoriaRepo iCategoriaRepo; 
-	@InjectMocks
+	
 	private ServicioCategoria servicioCategoria;
 	
-	private Categoria categoria;
+	private Categoria categoria1;
+	private Categoria categoria2;
 	
 	@BeforeEach
 	void inicio() throws DomainException {
-		categoria = new Categoria();
-		categoria.setId_categoria(5);
-		categoria.setCat_nombre("Normal");
-		categoria.setCat_descripcion("Descripcion categoria");
+		iCategoriaRepo = mock(ICategoriaRepo.class);
+		servicioCategoria = mock(ServicioCategoria.class);
+		categoria1 = new Categoria();
+		categoria1.setId_categoria(5);
+		categoria1.setCat_nombre("Normal");
+		categoria1.setCat_descripcion("Descripcion categoria");
+	
+		categoria2 = new Categoria();
+		categoria2.setId_categoria(10);
+		categoria2.setCat_nombre("Extra");
+		categoria2.setCat_descripcion("Descripcion categoria dos");
+	
 	}
 	
 	
 	@Test
 	void testLeerUno() {
-
+		when(servicioCategoria.leerUno(5)).thenReturn(categoria1);	
+		Categoria res = servicioCategoria.leerUno(5);
+		Assertions.assertEquals(res,categoria1);
+	
 	}
 
+	@Test
+	void testLeerUno_2() {
+		when(servicioCategoria.leerUno(5)).thenReturn(categoria1);	
+		Categoria res = servicioCategoria.leerUno(10);
+		Assertions.assertNotEquals(res,categoria1);
+	
+	}
 	@Test
 	void testLeerTodos() {
-		fail("Not yet implemented");
+		when(servicioCategoria.leerTodos()).thenReturn(List.of(categoria1,categoria2));
+		List<Categoria> listaCat = servicioCategoria.leerTodos();
+		Assertions.assertEquals(2,listaCat.size());
+		
 	}
-
 	@Test
-	void testActualizar() {
-		fail("Not yet implemented");
+	void testLeerTodos_2() {
+		when(servicioCategoria.leerTodos()).thenReturn(List.of(categoria1,categoria2));
+		List<Categoria> listaCat = servicioCategoria.leerTodos();
+		Assertions.assertNotEquals(1,listaCat.size());
+		
 	}
 
-	@Test
-	void testDeleteCategoria() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testDeleteInt() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testInsert() {
-		fail("Not yet implemented");
-	}
-
+	
 }
